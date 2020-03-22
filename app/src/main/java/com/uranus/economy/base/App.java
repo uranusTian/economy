0bcd4997df88f03c199f8790a7a3b24856a0ed37
uranusTian <1657513085@qq.com>
2020-03-22 23:06:52 +0800

    delete ixintui

--- app/src/main/java/com/uranus/economy/base/App.java
+++ app/src/main/java/com/uranus/economy/base/App.java
@@ -9,6 +9,8 @@ import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.uranus.economy.ActivityLifecycleCallback;
+import com.uranus.economy.util.CrashHandler;
 import com.uranus.economy.util.LogUtils;
 
 
@@ -42,44 +44,44 @@ public class App extends MultiDexApplication {
 //        DBManager.getInstance();
         //Activity生命周期监听注册
         registerActivityLifecycleCallbacks(ActivityLifecycleCallback.getInstance());
-        if (isMainProcess()){
-            registerBroadcast();
-            initIXinTui();
-        }
+//        if (isMainProcess()){
+//            registerBroadcast();
+////            initIXinTui();
+//        }
     }
 
-    public static void initIXinTui(){
-        PushSdkApi.register(context, BuildConfig.IXINTUI_APP_KEY, BuildConfig.IXINTUI_CHANNEL, AppUtils.getVersionName(context),
-                BuildConfig.XIAOMI_APPID, BuildConfig.XIAOMI_AppKey, BuildConfig.MEIZU_APPID, BuildConfig.MEIZU_APPKEY);
-    }
+//    public static void initIXinTui(){
+//        PushSdkApi.register(context, BuildConfig.IXINTUI_APP_KEY, BuildConfig.IXINTUI_CHANNEL, AppUtils.getVersionName(context),
+//                BuildConfig.XIAOMI_APPID, BuildConfig.XIAOMI_AppKey, BuildConfig.MEIZU_APPID, BuildConfig.MEIZU_APPKEY);
+//    }
 
 
-    private void registerBroadcast() {
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("com.ixintui.action.MESSAGE");
-        intentFilter.addAction("com.ixintui.action.RESULT");
-        intentFilter.addAction("com.ixintui.action.notification.CLICK");
-        PushReceiver pushReceiver = new PushReceiver();
-        registerReceiver(pushReceiver, intentFilter);
-
-        //targesdkversion大于等于26的时候需要动态注册AndroidMainfest中的广播
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("com.ixintui.action.BROADCAST");
-        filter.addAction("android.intent.action.BOOT_COMPLETED");
-        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        filter.addAction("android.intent.action.REBOOT");
-        filter.addAction("android.intent.action.ACTION_SHUTDOWN");
-        filter.addCategory("android.intent.category.HOME");
-        filter.addAction("android.intent.action.MEDIA_MOUNTED");
-        filter.addAction("android.intent.action.MEDIA_UNMOUNTED");
-        Receiver receiver = new Receiver();
-        registerReceiver(receiver, filter);
-
-        IntentFilter ixintuifilter = new IntentFilter();
-        ixintuifilter.addAction("android.intent.action.PACKAGE_REMOVED");
-        ixintuifilter.addDataScheme("package");
-        registerReceiver(receiver,ixintuifilter);
-    }
+//    private void registerBroadcast() {
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction("com.ixintui.action.MESSAGE");
+//        intentFilter.addAction("com.ixintui.action.RESULT");
+//        intentFilter.addAction("com.ixintui.action.notification.CLICK");
+//        PushReceiver pushReceiver = new PushReceiver();
+//        registerReceiver(pushReceiver, intentFilter);
+//
+//        //targesdkversion大于等于26的时候需要动态注册AndroidMainfest中的广播
+//        IntentFilter filter = new IntentFilter();
+//        filter.addAction("com.ixintui.action.BROADCAST");
+//        filter.addAction("android.intent.action.BOOT_COMPLETED");
+//        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        filter.addAction("android.intent.action.REBOOT");
+//        filter.addAction("android.intent.action.ACTION_SHUTDOWN");
+//        filter.addCategory("android.intent.category.HOME");
+//        filter.addAction("android.intent.action.MEDIA_MOUNTED");
+//        filter.addAction("android.intent.action.MEDIA_UNMOUNTED");
+//        Receiver receiver = new Receiver();
+//        registerReceiver(receiver, filter);
+//
+//        IntentFilter ixintuifilter = new IntentFilter();
+//        ixintuifilter.addAction("android.intent.action.PACKAGE_REMOVED");
+//        ixintuifilter.addDataScheme("package");
+//        registerReceiver(receiver,ixintuifilter);
+//    }
 
     /**
      * 包名判断是否为主进程
